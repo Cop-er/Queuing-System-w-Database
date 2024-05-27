76e439a9c969b2138d49b5864a9a69e4257d379f
floiwebsite <renren.gerald@gmail.com>
2024-05-27 13:10:58 +0800

    Labels and Database Total <clientReceiving> : added the label and total per transaction of tables and save it to database.

--- ClientReceiving/ReceivingPrint.Designer.cs
+++ ClientReceiving/ReceivingPrint.Designer.cs
@@ -29,122 +29,236 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.button5, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button7, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button6, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button4, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.label7, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(43, 87);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(857, 427);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(677, 444);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // label7
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(208, 207);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Birth Registration and Out of Town Registration";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.label7.Location = new System.Drawing.Point(341, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button2
+            // label6
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(217, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(208, 207);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Death Registration";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.label6.Location = new System.Drawing.Point(172, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button3
+            // label5
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.Location = new System.Drawing.Point(431, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(208, 207);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Marriage Registration / Application";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.label5.Location = new System.Drawing.Point(3, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button4
+            // label4
             // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(645, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(209, 207);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Certified True Copy and Negative Certification";
-            this.button4.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.label4.Location = new System.Drawing.Point(510, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // button6
+            // label3
             // 
-            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button6.Location = new System.Drawing.Point(3, 216);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(208, 208);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Acknowlegdement , Legitimation and Edorsements to PSA";
-            this.button6.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.label3.Location = new System.Drawing.Point(341, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.label2.Location = new System.Drawing.Point(172, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button7
             // 
             this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button7.Location = new System.Drawing.Point(217, 216);
+            this.button7.Location = new System.Drawing.Point(341, 255);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(208, 208);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "Filing of Petition for Correction";
+            this.button7.Size = new System.Drawing.Size(163, 186);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Court Orders / Decrees";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(172, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(163, 186);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Filing of Petition for Correction";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
             // 
             // button5
             // 
             this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button5.Location = new System.Drawing.Point(431, 216);
+            this.button5.Location = new System.Drawing.Point(3, 255);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(208, 208);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "Court Orders / Decrees";
+            this.button5.Size = new System.Drawing.Size(163, 186);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Acknowlegdement , Legitimation and Edorsements to PSA";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(510, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 186);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Certified True Copy and Negative Certification";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(341, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 186);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Marriage Registration / Application";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(172, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 186);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Death Registration";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 186);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Birth Registration and Out of Town Registration";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Total : 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ReceivingPrint
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 427);
+            this.ClientSize = new System.Drawing.Size(762, 561);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "ReceivingPrint";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Client Receiving";
+            this.Load += new System.EventHandler(this.ReceivingPrint_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -152,13 +266,20 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ClientReceiving/ReceivingPrint.cs
+++ ClientReceiving/ReceivingPrint.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace ClientReceiving
 {
@@ -15,6 +17,160 @@ namespace ClientReceiving
         public ReceivingPrint()
         {
             InitializeComponent();
+        }
+
+        private MongoClient clientDatabase;
+        private IMongoDatabase Database;
+        private DateTime dt = DateTime.Now;
+
+        private int t1 = 0;
+        private int t2 = 0;
+        private int t3 = 0;
+        private int t4 = 0;
+        private int t5 = 0;
+        private int t6 = 0;
+        private int t7 = 0;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           incrementNumber(1);
+        }
+
+        private async void incrementNumber(int index)
+        {
+            try
+            {
+                string labelName = "label" + index;
+                string counterName = "t" + index;
+                string tableNumber = "Table " + index;
+
+                var counterField = this.GetType().GetField(counterName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                int counterValue = (int)counterField.GetValue(this);
+                counterValue++;
+                counterField.SetValue(this, counterValue);
+
+                await UpdateData(dt.ToShortDateString());
+                Label label = this.Controls.Find(labelName, true).FirstOrDefault() as Label;
+                label.Text = $"{tableNumber}   <Total Clients : {counterValue}>";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private async void ReceivingPrint_Load(object sender, EventArgs e)
+        {
+
+            label1.Text = "Total = 0";
+            label2.Text = "Total = 0";
+            label3.Text = "Total = 0";
+            label4.Text = "Total = 0";
+            label5.Text = "Total = 0";
+            label6.Text = "Total = 0";
+            label7.Text = "Total = 0";
+
+            try { 
+            MongodbConnection con = new MongodbConnection();
+            con.ConnectToMongoDB();
+            clientDatabase = con.GetClient();
+            Database = con.GetDatabase();
+            await con.SaveData();
+            await RetriveData();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                label1.Text = $"Total = {t1}";
+                label2.Text = $"Total = {t2}";
+                label3.Text = $"Total = {t3}";
+                label4.Text = $"Total = {t4}";
+                label5.Text = $"Total = {t5}";
+                label6.Text = $"Total = {t6}";
+                label7.Text = $"Total = {t7}";
+            }
+        }
+
+        public async Task<bool> RetriveData()
+        {
+            var collection = Database.GetCollection<BsonDocument>(MongodbConnection.CollectionName);
+            var filter = Builders<BsonDocument>.Filter.Eq("DateString", dt.ToShortDateString());
+            var document = await collection.Find(filter).FirstOrDefaultAsync();
+
+            t1 = document.GetValue("EmesilBirth", "").AsInt32;
+            t2 = document.GetValue("JomaryDeath", "").AsInt32;
+            t3 = document.GetValue("HelenMarriage", "").AsInt32;
+            t4 = document.GetValue("NikkiCTC", "").AsInt32;
+            t5 = document.GetValue("DonCourt", "").AsInt32;
+            t6 = document.GetValue("NikkiLegitimationEdorsementsLegitimation", "").AsInt32;
+            t7 = document.GetValue("FrechieCorrection", "").AsInt32;
+
+            return true;
+        }
+
+        public async Task UpdateData(string dtx)
+        {
+            var collection = Database.GetCollection<BsonDocument>(MongodbConnection.CollectionName);
+            var filter = Builders<BsonDocument>.Filter.Eq("DateString", dtx);
+            var update = Builders<BsonDocument>.Update
+                .Set("EmesilBirth", t1)
+                .Set("JomaryDeath", t2)
+                .Set("HelenMarriage", t3)
+                .Set("NikkiCTC", t4)
+                .Set("DonCourt", t5)
+                .Set("NikkiLegitimationEdorsementsLegitimation", t6)
+                .Set("FrechieCorrection", t7);
+
+            var result = await collection.UpdateOneAsync(filter, update);
+            if (result.IsAcknowledged && result.ModifiedCount > 0)
+            {
+                Console.WriteLine("Document updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("No documents matched the filter or no modifications were made.");
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            incrementNumber(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            incrementNumber(3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            incrementNumber(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            incrementNumber(5);
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            incrementNumber(7);
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            incrementNumber(6);
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            incrementNumber(5);
         }
     }
 }
